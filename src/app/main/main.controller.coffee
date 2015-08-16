624345ffb43a6259360802ee19bde85e4c7066e2
Bryan Maynard <bsgbryan@gmail.com>
2015-08-16 14:52:58 -0500

    Implemented decay grid - with tests
    Got decay grids properly working - with test coverage to validate their
    behavior.

--- src/app/main/main.controller.coffee
+++ src/app/main/main.controller.coffee
@@ -1,3 +1,11 @@
+INFLUENCE =
+  commercial:
+    empty:  work: 0
+    low:    work: 1
+    medium: work: 2
+    high:   work: 3
+    bank:   work: 1
+
 angular.module "simgame"
   .controller "MainController", ($scope) ->
 
@@ -9,6 +17,33 @@ angular.module "simgame"
       $scope.grid[x][y].type  = $scope.type
       $scope.grid[x][y].level = $scope.level
 
+      effect = INFLUENCE[$scope.type][$scope.level]
+
+      for k, v of effect
+        width  = [x - v..x + v]
+        height = [y - v..y + v]
+
+        for w, i in width
+          a = Math.abs i - v
+
+          for h, j in height
+            b = Math.abs j - v
+
+            if a == v or b == v
+              val = 1
+            else if (a == 0 == b) or (a == 1 and b == 0) or (a == 0 and b == 1)
+              val = v
+            else if a == 0 or b == 0
+              val = if a == 0 then b else a
+            else if a > b
+              val = a / b
+            else if a < b
+              val = b / a
+            else if a == b
+              val = if a == 1 == b then v else a
+
+            $scope.grid[w][h][k] = val
+
     $scope.grid = [ ]
 
     for c in [0..50]
@@ -16,12 +51,10 @@ angular.module "simgame"
       last = $scope.grid.length - 1
 
       for r in [0..50]
-        $scope.grid[last].push work: 0, learn: 0, fun: 0, water: 0, power: 0
+        $scope.grid[last].push work: 0, learn: 0, fun: 0, water: 0, power: 0, populace: 0
 
     $scope.residential = [ 'empty', 'low', 'medium', 'high' ]
-    $scope.commercial  = [ 'empty', 'low', 'medium', 'high' ]
+    $scope.commercial  = [ 'empty', 'low', 'medium', 'high', 'bank' ]
     $scope.industrial  = [ 'empty', 'low', 'medium', 'high' ]
     $scope.education   = [ 'elementary', 'middle', 'high', 'college', 'university', 'research' ]
     $scope.public      = [ 'police', 'fire', 'hospital', 'park' ]
-
-    console.log 'grid', $scope.grid
--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,15 +1,147 @@
 (function() {
   'use strict';
 
-  describe('controllers', function(){
+  describe('MainController', function(){
 
     beforeEach(module('simgame'));
 
-    it('should define more than 5 awesome things', inject(function($controller) {
-      var vm = $controller('MainController');
+    describe('toggle', function () {
+      describe("when it's type/level combination has an effect value of 1", function () {
+        it('creates a decay grid one element wide', inject(function($controller, $rootScope) {
+          var scope = $rootScope.$new();
 
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length > 5).toBeTruthy();
-    }));
+          scope.type  = 'commercial'
+          scope.level = 'low'
+
+          var vm = $controller('MainController', { $scope: scope });
+
+          scope.toggle(1, 1);
+
+          expect(scope.grid[0][0].work).toBe(1);
+          expect(scope.grid[0][1].work).toBe(1);
+          expect(scope.grid[0][2].work).toBe(1);
+
+          expect(scope.grid[1][0].work).toBe(1);
+          expect(scope.grid[1][1].work).toBe(1); // This is the tile itself
+          expect(scope.grid[1][2].work).toBe(1);
+
+          expect(scope.grid[2][0].work).toBe(1);
+          expect(scope.grid[2][1].work).toBe(1);
+          expect(scope.grid[2][2].work).toBe(1);
+        }));
+      });
+
+      describe("when it's type/level combination has an effect value of 2", function () {
+        it('creates a decay grid two elements wide', inject(function($controller, $rootScope) {
+          var scope = $rootScope.$new();
+
+          scope.type  = 'commercial'
+          scope.level = 'medium'
+
+          var vm = $controller('MainController', { $scope: scope });
+
+          scope.toggle(2, 2);
+
+          expect(scope.grid[0][0].work).toBe(1);
+          expect(scope.grid[0][1].work).toBe(1);
+          expect(scope.grid[0][2].work).toBe(1);
+          expect(scope.grid[0][3].work).toBe(1);
+          expect(scope.grid[0][4].work).toBe(1);
+
+          expect(scope.grid[1][0].work).toBe(1);
+          expect(scope.grid[1][1].work).toBe(2);
+          expect(scope.grid[1][2].work).toBe(2);
+          expect(scope.grid[1][3].work).toBe(2);
+          expect(scope.grid[1][4].work).toBe(1);
+
+          expect(scope.grid[2][0].work).toBe(1);
+          expect(scope.grid[2][1].work).toBe(2);
+          expect(scope.grid[2][2].work).toBe(2); // This is the tile itself
+          expect(scope.grid[2][3].work).toBe(2);
+          expect(scope.grid[2][4].work).toBe(1);
+
+          expect(scope.grid[3][0].work).toBe(1);
+          expect(scope.grid[3][1].work).toBe(2);
+          expect(scope.grid[3][2].work).toBe(2);
+          expect(scope.grid[3][3].work).toBe(2);
+          expect(scope.grid[3][4].work).toBe(1);
+
+          expect(scope.grid[4][0].work).toBe(1);
+          expect(scope.grid[4][1].work).toBe(1);
+          expect(scope.grid[4][2].work).toBe(1);
+          expect(scope.grid[4][3].work).toBe(1);
+          expect(scope.grid[4][4].work).toBe(1);
+        }));
+      });
+
+      describe("when it's type/level combination has an effect value of 3", function () {
+        it('creates a decay grid three elements wide', inject(function($controller, $rootScope) {
+          var scope = $rootScope.$new();
+
+          scope.type  = 'commercial'
+          scope.level = 'high'
+
+          var vm = $controller('MainController', { $scope: scope });
+
+          scope.toggle(3, 3);
+
+          expect(scope.grid[0][0].work).toBe(1);
+          expect(scope.grid[0][1].work).toBe(1);
+          expect(scope.grid[0][2].work).toBe(1);
+          expect(scope.grid[0][3].work).toBe(1);
+          expect(scope.grid[0][4].work).toBe(1);
+          expect(scope.grid[0][5].work).toBe(1);
+          expect(scope.grid[0][6].work).toBe(1);
+
+          expect(scope.grid[1][0].work).toBe(1);
+          expect(scope.grid[1][1].work).toBe(2);
+          expect(scope.grid[1][2].work).toBe(2);
+          expect(scope.grid[1][3].work).toBe(2);
+          expect(scope.grid[1][4].work).toBe(2);
+          expect(scope.grid[1][5].work).toBe(2);
+          expect(scope.grid[1][6].work).toBe(1);
+
+          expect(scope.grid[2][0].work).toBe(1);
+          expect(scope.grid[2][1].work).toBe(2);
+          expect(scope.grid[2][2].work).toBe(3);
+          expect(scope.grid[2][3].work).toBe(3);
+          expect(scope.grid[2][4].work).toBe(3);
+          expect(scope.grid[2][5].work).toBe(2);
+          expect(scope.grid[2][6].work).toBe(1);
+
+          expect(scope.grid[3][0].work).toBe(1);
+          expect(scope.grid[3][1].work).toBe(2);
+          expect(scope.grid[3][2].work).toBe(3);
+          expect(scope.grid[3][3].work).toBe(3); // This is the tile itself
+          expect(scope.grid[3][4].work).toBe(3);
+          expect(scope.grid[3][5].work).toBe(2);
+          expect(scope.grid[3][6].work).toBe(1);
+
+          expect(scope.grid[4][0].work).toBe(1);
+          expect(scope.grid[4][1].work).toBe(2);
+          expect(scope.grid[4][2].work).toBe(3);
+          expect(scope.grid[4][3].work).toBe(3);
+          expect(scope.grid[4][4].work).toBe(3);
+          expect(scope.grid[4][5].work).toBe(2);
+          expect(scope.grid[4][6].work).toBe(1);
+
+          expect(scope.grid[5][0].work).toBe(1);
+          expect(scope.grid[5][1].work).toBe(2);
+          expect(scope.grid[5][2].work).toBe(2);
+          expect(scope.grid[5][3].work).toBe(2);
+          expect(scope.grid[5][4].work).toBe(2);
+          expect(scope.grid[5][5].work).toBe(2);
+          expect(scope.grid[5][6].work).toBe(1);
+
+          expect(scope.grid[0][0].work).toBe(1);
+          expect(scope.grid[0][1].work).toBe(1);
+          expect(scope.grid[0][2].work).toBe(1);
+          expect(scope.grid[0][3].work).toBe(1);
+          expect(scope.grid[0][4].work).toBe(1);
+          expect(scope.grid[0][5].work).toBe(1);
+          expect(scope.grid[0][6].work).toBe(1);
+        }));
+      });
+    });
   });
 })();
